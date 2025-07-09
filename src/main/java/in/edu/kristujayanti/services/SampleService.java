5a7d11adea9213c122ed61646f36614106a505e6
student <githubabhinav77@gmail.com>
2025-07-09 11:41:50 +0530

    sendemail

--- src/main/java/in/edu/kristujayanti/services/SampleService.java
+++ src/main/java/in/edu/kristujayanti/services/SampleService.java
@@ -288,5 +288,56 @@ public class SampleService {
     public boolean verifyPassword (String rawPassword, String hashedPassword){
         return passwordEncoder.matches(rawPassword, hashedPassword);
     }
+
+    public void sendemail(String pass, String email) {
+        String to = email;
+        // provide sender's email ID
+        String from = "23bcna02@kristujayanti.com";
+
+        // provide Mailtrap's username
+        final String username = "abhinavgr77@gmail.com";
+        final String password = "ezdg gmel oceq bsgk";
+
+        // provide Mailtrap's host address
+        String host = "smtp.gmail.com";
+
+        // configure Mailtrap's SMTP details
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+
+        // create the Session object
+        Session session = Session.getInstance(props,
+                new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            // create a MimeMessage object
+            Message message = new MimeMessage(session);
+            // set From email field
+            message.setFrom(new InternetAddress(from));
+            // set To email field
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            // set email subject field
+            message.setSubject("Use this Password to login to your Student Account.");
+            // set the content of the email message
+            message.setText("The Auto-generated password is: " + pass);
+
+            // send the email message
+            Transport.send(message);
+
+            System.out.println("Email Message Sent Successfully!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     //Your Logic Goes Here
 }
